b83cc4b0e17db6eb04d2337931c6e1acc1c0f08c
Krygin Ivan <krygin.ia@gmail.com>
2019-11-14 01:15:07 +0300

    brown robinson kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,76 +1,87 @@
 import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
 import kotlin.random.Random
 
 fun main(arguments: Array<String>) {
     val n = 10
-    val m = 10
-    val defenderPaymentMatrix = Array(n) {IntArray(m) {Random.nextInt(0, 100)} }
-    val attackerPaymentMatrix = Array(n) {IntArray(m) {Random.nextInt(0, 100)} }
+    val m = 5
+    val defenderPaymentMatrix = Array(m) {Array(n) {Random.nextDouble(0.0, 100.0)} }
+    val attackerPaymentMatrix = Array(m) {Array(n) {Random.nextDouble(0.0, 100.0)} }
 
+//    defenderPaymentMatrix.print()
+//
+//    println("Row Example")
+//    defenderPaymentMatrix.row(2).print()
+//
+//    println("Column Example")
+//    defenderPaymentMatrix.column(2).print()
 
     playMatrixGame(n, m)
-
+//
     print("Hello world!!!")
 }
 
 fun playMatrixGame(n: Int, m: Int) {
-    val paymentMatrix = Array(n) {Array(m) {Random.nextDouble(0.0, 100.0)} }
+//    val paymentMatrix = Array(n) {Array(m) {Random.nextDouble(0.0, 100.0)} }
 
-    val pX = Array(n) {0}
-    val pY = Array(m) {0}
+    // n = 4
+    // m = 5
+    val paymentMatrix = arrayOf(
+        arrayOf(4.0, 1.0, -2.0, -1.0, 0.0),
+        arrayOf(2.0, 3.0, 2.0, 0.0 , 1.0),
+        arrayOf(1.0, 0.0, 2.0, 3.0, 2.0),
+        arrayOf(0.0, -1.0, -2.0, 1.0, 4.0)
+    )
 
-    val pV1 = Array(n) { 0.0 }
-    val pV2 = Array(m) { 0.0 }
+    // Накопленный выигрыш
+//    val winX = Array(n) { 0.0 }
+//    val winY = Array(m) { 0.0 }
 
-    val maxMin = getMaxMin(paymentMatrix)
+    val winX = arrayOf(0.0, 0.0, 0.0, 0.0, 0.0)
+    val winY = arrayOf(0.0, 0.0, 0.0, 0.0)
 
-    pX[maxMin.i]++
+    // step 0
+    var strategyX = 0
+    var strategyY = 0
 
-    for(i in 0 until m) pV2[i] += paymentMatrix[maxMin.i][i]
+    var k = 0
 
-    var k1 = 1
-    var k2 = 0
-
-    var vMin = 0.0
-    var vMax = 0.0
+    print("k".padStart(4))
+    print("x".padStart(2))
+    print("y".padStart(2))
+    for (i in 0 until winX.size) print("x$i".padStart(4))
+    for (i in 0 until winY.size) print("y$i".padStart(4))
+    println()
 
     do {
-        // Выбор второго игрока
-        var min = Double.MAX_VALUE
-        var iMin = 0
-        for (i in 0 until m)
-            if (pV2[i] < min) {
-                min = pV2[i]
-                iMin = i
-            }
-        pY[iMin]++
-        k2++
-        vMin = min / k1 // Верхняя цена игры
-        for (i in 0 until n) pV1[i] += paymentMatrix[i][iMin]
+        k++;
+        for (i in 0 until winX.size) {
+            winX[i] += paymentMatrix[strategyY][i]
+        }
+        for (i in 0 until winY.size) {
+            winY[i] += paymentMatrix[i][strategyX]
+        }
 
-        var max = 0.0
-        var iMax = 0
-        for (i in 0 until n)
-            if (pV1[i] > max) {
-                max = pV1[i]
-                iMax = i
-            }
-        pX[iMax]++
-        k1++
-        vMax = max / k2 // Нижняя цена игры
-        for (i in 0 until m) pV2[i] += paymentMatrix[iMax][i]
-        println("maxValue = $vMax minValue = $vMin delta = ${(vMax - vMin).absoluteValue}")
+        val max = winX.max()!!
+        val min = winY.min()!!
 
-    } while ((vMax - vMin).absoluteValue > 0.001)
+        val xAverageWin = max/k.toDouble()
+        val yAverageWin = min/k.toDouble()
 
-    val sumX = pX.sum().toDouble()
-    pX.forEach { print("${it/sumX} ") }
-    println()
+        print(k.toString().padStart(6))
+        print(strategyX.toString().padStart(2))
+        print(strategyY.toString().padStart(2))
+        winX.forEach { print(it.toInt().toString().padStart(10)) }
+        winY.forEach { print(it.toInt().toString().padStart(10)) }
+        print(String.format("%.2f", xAverageWin).padStart(10))
+        print(String.format("%.2f", yAverageWin).padStart(10))
+        println()
 
-    val sumY = pY.sum().toDouble()
-    pY.forEach { print("${it/sumY} ") }
-    println()
+        strategyX = winX.indexOf(max)
+        strategyY = winY.indexOf(min)
 
+
+    } while (k < 8000)
 }
 
 fun getMaxMin(matrix: Array<Array<Double>>): MaxMin {
@@ -91,6 +102,26 @@ fun getMaxMin(matrix: Array<Array<Double>>): MaxMin {
 
     }
     return MaxMin(max, iMax)
+}
+
+fun Array<Array<Double>>.row(index: Int) = this[index]
+
+fun Array<Array<Double>>.column(index: Int) = flatMap { listOf(it[index]) }.toTypedArray()
+
+fun Array<Double>.print() {
+    for(i in 0 until size) {
+        print(this[i].roundToInt().toString().padStart(4))
+    }
+    println()
+}
+
+fun Array<Array<Double>>.print() {
+    for (i in 0 until size) {
+        for (j in 0 until this[i].size) {
+            print(this[i][j].roundToInt().toString().padStart(4))
+        }
+        println()
+    }
 }
 
 data class MaxMin(val value: Double, val i: Int) {
