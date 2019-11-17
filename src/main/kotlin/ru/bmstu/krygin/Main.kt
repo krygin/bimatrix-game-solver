cdb355e3724c72a917a2012898ed3a68090cece8
Krygin Ivan <krygin.ia@gmail.com>
2019-11-17 18:34:48 +0300

    brown robinson kotlin

--- src/main/kotlin/ru/bmstu/krygin/Main.kt
+++ src/main/kotlin/ru/bmstu/krygin/Main.kt
@@ -1,13 +1,14 @@
 package ru.bmstu.krygin
 
+import java.io.File
 import kotlin.math.roundToInt
 import kotlin.random.Random
 
 fun main(arguments: Array<String>) {
     val n = 10
     val m = 5
-    val defenderPaymentMatrix = Array(m) {Array(n) {Random.nextDouble(0.0, 100.0)} }
-    val attackerPaymentMatrix = Array(m) {Array(n) {Random.nextDouble(0.0, 100.0)} }
+    val defenderPaymentMatrix = Array(m) { Array(n) { Random.nextDouble(0.0, 100.0) } }
+    val attackerPaymentMatrix = Array(m) { Array(n) { Random.nextDouble(0.0, 100.0) } }
 
 //    defenderPaymentMatrix.print()
 //
@@ -29,7 +30,7 @@ fun playMatrixGame(n: Int, m: Int) {
     // m = 5
     val paymentMatrix = arrayOf(
         arrayOf(4.0, 1.0, -2.0, -1.0, 0.0),
-        arrayOf(2.0, 3.0, 2.0, 0.0 , 1.0),
+        arrayOf(2.0, 3.0, 2.0, 0.0, 1.0),
         arrayOf(1.0, 0.0, 2.0, 3.0, 2.0),
         arrayOf(0.0, -1.0, -2.0, 1.0, 4.0)
     )
@@ -48,7 +49,19 @@ fun playMatrixGame(n: Int, m: Int) {
     var k = 0
 
 
-
+    val fileWriter = File("matrix_game.csv").printWriter()
+    fileWriter.apply {
+        val columns = listOf(
+            "k",
+            "x",
+            "y",
+            *winX.indices.map { "x$it" }.toTypedArray(),
+            *winY.indices.map { "y$it" }.toTypedArray(),
+            "minValue",
+            "maxValue"
+        )
+        println(columns.joinToString())
+    }
     print("k".padStart(4))
     print("x".padStart(2))
     print("y".padStart(2))
@@ -68,8 +81,8 @@ fun playMatrixGame(n: Int, m: Int) {
         val max = winX.max()!!
         val min = winY.min()!!
 
-        val xAverageWin = max/k.toDouble()
-        val yAverageWin = min/k.toDouble()
+        val xAverageWin = max / k.toDouble()
+        val yAverageWin = min / k.toDouble()
 
         print(k.toString().padStart(6))
         print(strategyX.toString().padStart(2))
@@ -80,11 +93,26 @@ fun playMatrixGame(n: Int, m: Int) {
         print(String.format("%.2f", yAverageWin).padStart(10))
         println()
 
+        fileWriter.apply {
+            val values = listOf(
+                k.toString(),
+                strategyX.toString(),
+                strategyY.toString(),
+                *winX.map { it.toString() }.toTypedArray(),
+                *winY.map { it.toString() }.toTypedArray(),
+                xAverageWin.toString(),
+                yAverageWin.toString()
+                )
+            println(values.joinToString())
+        }
+
         strategyX = winX.indexOf(max)
         strategyY = winY.indexOf(min)
 
 
     } while (k < 8000)
+
+    fileWriter.close()
 }
 
 fun Array<Array<Double>>.row(index: Int) = this[index]
@@ -92,7 +120,7 @@ fun Array<Array<Double>>.row(index: Int) = this[index]
 fun Array<Array<Double>>.column(index: Int) = flatMap { listOf(it[index]) }.toTypedArray()
 
 fun Array<Double>.print() {
-    for(i in 0 until size) {
+    for (i in 0 until size) {
         print(this[i].roundToInt().toString().padStart(4))
     }
     println()
